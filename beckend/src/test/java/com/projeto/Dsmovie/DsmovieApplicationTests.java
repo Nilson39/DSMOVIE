2b300337aa33cebd91ca780389317915a27ff4c5
Nilson Ernande <nilson_ernande@hotmail.com>
2022-01-10 10:59:55 -0300

    Project created

---
+++ beckend/src/test/java/com/projeto/Dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.Dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
